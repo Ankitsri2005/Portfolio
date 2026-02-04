8d62e1b62286e16f1cf8ce4b19620876bc150966
Ankitsri2005 <vkankit3@gmail.com>
2026-02-05 03:44:09 +0530

    Update (no structural change)

--- script.js
+++ script.js
@@ -20,34 +20,6 @@ ScrollReveal({
 
 
 ScrollReveal().reveal('.home-content, .heading', { origin: 'bottom' });
-ScrollReveal().reveal(' .about-img', { origin: 'left' });
-ScrollReveal().reveal('.about-content, .home-img, .skills-container,.projects-container', { origin: 'top' });
-ScrollReveal().reveal('.contact-icons, .contact-text', { origin: 'bottom' });
-ScrollReveal().reveal(' ', { origin: 'right' });
-
-
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-
-
-menuIcon.onclick = () => {
-  navbar.classList.toggle('active');
-};
-
-
-
-ScrollReveal({
-  reset: true,
-  distance: '60px',
-  duration: 2000,
-  delay: 200
-});
-
-
-
-
-ScrollReveal().reveal('.home-content, .heading', { origin: 'bottom' });
 ScrollReveal().reveal('.home-img, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.about-content, .skills-container, .projects-container', { origin: 'top' });
 ScrollReveal().reveal('.contact-icons, .contact-text', { origin: 'bottom' });
